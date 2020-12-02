d5107fb9e186418f0f4f8526771686cfe3cfb868
altf4m88 <fadhilmauladhani@gmail.com>
2020-12-02 21:12:26 +0700

    C:/Program Files/Git/api/short/new code

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const shortId = require('shortid');
+const validUrl = require('valid-url');
 const db = mongoose.connection;
 const app = express();
 
@@ -35,6 +36,34 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/short/new', async (req, res) => {
+  const url = req.body.url;
+  const urlCode = shortId.generate();
+  if(!validUrl.isWebUrl(url)){
+    res.status(401).json({
+      error : 'INVALID URL'
+    })
+  } else {
+    try{
+      let findUrl = await URL.findOne({ original : url});
+
+      if(findUrl){
+        res.json({original : findUrl.original, shortened : findUrl.shortened});
+      } else {
+        findUrl = new URL({
+          original : url,
+          shortened : urlCode
+        });
+        await findOne.save();
+        res.json({original: findOne.original, shortened : findOne.shortened});
+      }
+    } catch(err){
+      console.error(err);
+      res.status(500).json('Server error...');
+    }
+  }
 });
 
 app.listen(port, function() {
